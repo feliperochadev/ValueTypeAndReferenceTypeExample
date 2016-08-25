6cad794d47b79afbf8a5a05e1048ff5fa7e95cc4
Felipe Rocha <feliperocha.ti@gmail.com>
2016-08-25 11:55:52 -0300

    Initial Commit and test value type in a collection with ChangeObjectOfCollectionWithValueType() method

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ValueTypeAndReferenceTypeExampleMain
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ChangeObjectOfCollectionWithValueType();
+        }
+
+        public static void ChangeObjectOfCollectionWithValueType()
+        {
+            var listInt = new List<int>() {1, 2, 3};
+            foreach(var item in listInt)
+            {
+                Console.WriteLine(item);
+            }
+            var val = listInt.First(x => x == 2);
+            val = 4;
+            Console.WriteLine("Afer change value 2 to 4 into a variable");
+            foreach(var item in listInt)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
