374ed3a2feb50bca241ec70cfc87b5c71ed601ac
Felipe Rocha <feliperocha.ti@gmail.com>
2016-08-25 12:16:59 -0300

    Add ChangeObjectOfCollectionWithReferenceType() method and minor changes

--- Program.cs
+++ Program.cs
@@ -9,22 +9,45 @@ namespace ValueTypeAndReferenceTypeExampleMain
         public static void Main(string[] args)
         {
             ChangeObjectOfCollectionWithValueType();
+            Console.WriteLine();
+            ChangeObjectOfCollectionWithReferenceType();
         }
 
         public static void ChangeObjectOfCollectionWithValueType()
         {
-            var listInt = new List<int>() {1, 2, 3};
-            foreach(var item in listInt)
+            var intList = new List<int>() {1, 2, 3};
+            foreach(var item in intList)
             {
                 Console.WriteLine(item);
             }
-            var val = listInt.First(x => x == 2);
+            var val = intList.First(x => x == 2);
             val = 4;
-            Console.WriteLine("After change value 2 to 4 into a variable");
-            foreach(var item in listInt)
+            Console.WriteLine();
+            Console.WriteLine("After change value 2 to 4 into a value variable");
+            Console.WriteLine();
+            foreach(var item in intList)
             {
                 Console.WriteLine(item);
             }
         }
+
+        public static void ChangeObjectOfCollectionWithReferenceType()
+        {
+            var carList = new List<Car>() {new Car{Year = 1999, Name = "focus"}, new Car{Year = 2010, Name = "Camaro"}, new Car{Year = 2016, Name = "Mustang"}};
+            foreach(var item in carList)
+            {
+                Console.WriteLine($"{item.Name} - {item.Year}");
+            }
+            var val = carList.First(x => x.Year == 2010);
+            val.Name = "Corvette";
+            Console.WriteLine();
+            Console.WriteLine("After change value Camaro to Corvette into a reference variable");
+            Console.WriteLine();
+            foreach(var item in carList)
+            {
+                Console.WriteLine($"{item.Name} - {item.Year}");
+            }
+        }
+
     }
 }
